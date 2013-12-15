f3f2214db48069d467a92b219b8b075699b3bb12
hanson <hangzhong.yu@gmail.com>
2013-12-15 20:21:37 +0800

    new design

---
+++ assets/media/prototype/更多_files/axurerp_pagespecificscript.js
@@ -0,0 +1,95 @@
+﻿for(var i = 0; i < 44; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u21'] = 'center';document.getElementById('u22_img').tabIndex = 0;
+
+u22.style.cursor = 'pointer';
+$axure.eventManager.click('u22', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Home.html');
+
+}
+});
+gv_vAlignTable['u23'] = 'center';document.getElementById('u24_img').tabIndex = 0;
+
+u24.style.cursor = 'pointer';
+$axure.eventManager.click('u24', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('就业信息.html');
+
+}
+});
+gv_vAlignTable['u25'] = 'center';document.getElementById('u26_img').tabIndex = 0;
+
+u26.style.cursor = 'pointer';
+$axure.eventManager.click('u26', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('招聘会日历.html');
+
+}
+});
+gv_vAlignTable['u27'] = 'center';document.getElementById('u28_img').tabIndex = 0;
+
+u28.style.cursor = 'pointer';
+$axure.eventManager.click('u28', function(e) {
+
+if (true) {
+
+    self.location.href="resources/reload.html#" + encodeURI($axure.globalVariableProvider.getLinkUrl($axure.pageData.url));
+
+}
+});
+gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u33'] = 'center';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';document.getElementById('u6_img').tabIndex = 0;
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+	NewTab('#', "");
+
+}
+});
+gv_vAlignTable['u7'] = 'center';document.getElementById('u8_img').tabIndex = 0;
+
+u8.style.cursor = 'pointer';
+$axure.eventManager.click('u8', function(e) {
+
+if (true) {
+
+	NewTab('#', "");
+
+}
+});
+gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u43'] = 'center';document.getElementById('u10_img').tabIndex = 0;
+
+u10.style.cursor = 'pointer';
+$axure.eventManager.click('u10', function(e) {
+
+if (true) {
+
+	NewTab('#', "");
+
+}
+});
+gv_vAlignTable['u11'] = 'center';document.getElementById('u12_img').tabIndex = 0;
+
+u12.style.cursor = 'pointer';
+$axure.eventManager.click('u12', function(e) {
+
+if (true) {
+
+	NewTab('#', "");
+
+}
+});
+gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u19'] = 'center';
