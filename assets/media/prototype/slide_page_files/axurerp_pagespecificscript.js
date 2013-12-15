159ec9d1e74dc49d5f76a53e18499153d544b357
hanson <hangzhong.yu@gmail.com>
2013-12-15 20:17:14 +0800

    new design

--- assets/media/prototype/slide_page_files/axurerp_pagespecificscript.js
+++ assets/media/prototype/slide_page_files/axurerp_pagespecificscript.js
@@ -4,47 +4,25 @@ $axure.eventManager.pageLoad(
 function (e) {
 
 });
-document.getElementById('u50_img').tabIndex = 0;
+gv_vAlignTable['u51'] = 'center';gv_vAlignTable['u53'] = 'center';gv_vAlignTable['u55'] = 'center';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u59'] = 'center';document.getElementById('u20_img').tabIndex = 0;
 
-u50.style.cursor = 'pointer';
-$axure.eventManager.click('u50', function(e) {
-
-if (true) {
-
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
-
-}
-});
-gv_vAlignTable['u51'] = 'center';document.getElementById('u52_img').tabIndex = 0;
-
-u52.style.cursor = 'pointer';
-$axure.eventManager.click('u52', function(e) {
+u20.style.cursor = 'pointer';
+$axure.eventManager.click('u20', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
+	NewTab('#', "");
 
 }
 });
-gv_vAlignTable['u53'] = 'center';document.getElementById('u54_img').tabIndex = 0;
-
-u54.style.cursor = 'pointer';
-$axure.eventManager.click('u54', function(e) {
-
-if (true) {
-
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
-
-}
-});
-gv_vAlignTable['u55'] = 'center';gv_vAlignTable['u57'] = 'center';gv_vAlignTable['u59'] = 'center';gv_vAlignTable['u21'] = 'center';document.getElementById('u22_img').tabIndex = 0;
+gv_vAlignTable['u21'] = 'center';document.getElementById('u22_img').tabIndex = 0;
 
 u22.style.cursor = 'pointer';
 $axure.eventManager.click('u22', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
+	NewTab($axure.globalVariableProvider.getLinkUrl('就业信息.html'), "");
 
 }
 });
@@ -55,7 +33,7 @@ $axure.eventManager.click('u24', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_2.html'), "");
+	NewTab($axure.globalVariableProvider.getLinkUrl('招聘会日历.html'), "");
 
 }
 });
@@ -66,25 +44,14 @@ $axure.eventManager.click('u26', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_3.html'), "");
+	NewTab($axure.globalVariableProvider.getLinkUrl('更多.html'), "");
 
 }
 });
-gv_vAlignTable['u27'] = 'center';document.getElementById('u28_img').tabIndex = 0;
+gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u29'] = 'center';document.getElementById('u60_img').tabIndex = 0;
 
-u28.style.cursor = 'pointer';
-$axure.eventManager.click('u28', function(e) {
-
-if (true) {
-
-	NewTab($axure.globalVariableProvider.getLinkUrl('page_4.html'), "");
-
-}
-});
-gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u61'] = 'center';gv_vAlignTable['u63'] = 'center';gv_vAlignTable['u65'] = 'center';document.getElementById('u66_img').tabIndex = 0;
-
-u66.style.cursor = 'pointer';
-$axure.eventManager.click('u66', function(e) {
+u60.style.cursor = 'pointer';
+$axure.eventManager.click('u60', function(e) {
 
 if (true) {
 
@@ -92,14 +59,25 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u67'] = 'center';gv_vAlignTable['u69'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u33'] = 'center';gv_vAlignTable['u35'] = 'center';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u39'] = 'center';document.getElementById('u0_img').tabIndex = 0;
+gv_vAlignTable['u61'] = 'center';gv_vAlignTable['u63'] = 'center';gv_vAlignTable['u66'] = 'center';gv_vAlignTable['u68'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u33'] = 'center';gv_vAlignTable['u35'] = 'center';document.getElementById('u36_img').tabIndex = 0;
+
+u36.style.cursor = 'pointer';
+$axure.eventManager.click('u36', function(e) {
+
+if (true) {
+
+	NewTab($axure.globalVariableProvider.getLinkUrl('slide_page.html'), "");
+
+}
+});
+gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u70'] = 'center';document.getElementById('u0_img').tabIndex = 0;
 
 u0.style.cursor = 'pointer';
 $axure.eventManager.click('u0', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
+	NewTab('#', "");
 
 }
 });
@@ -110,29 +88,51 @@ $axure.eventManager.click('u8', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
+	NewTab('#', "");
 
 }
 });
-gv_vAlignTable['u9'] = 'center';document.getElementById('u42_img').tabIndex = 0;
+gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u43'] = 'center';document.getElementById('u44_img').tabIndex = 0;
 
-u42.style.cursor = 'pointer';
-$axure.eventManager.click('u42', function(e) {
+u44.style.cursor = 'pointer';
+$axure.eventManager.click('u44', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('slide_page.html'), "");
+	NewTab('#', "");
 
 }
 });
-gv_vAlignTable['u41'] = 'center';gv_vAlignTable['u45'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u49'] = 'center';document.getElementById('u10_img').tabIndex = 0;
+document.getElementById('u48_img').tabIndex = 0;
+
+u48.style.cursor = 'pointer';
+$axure.eventManager.click('u48', function(e) {
+
+if (true) {
+
+	NewTab('#', "");
+
+}
+});
+document.getElementById('u46_img').tabIndex = 0;
+
+u46.style.cursor = 'pointer';
+$axure.eventManager.click('u46', function(e) {
+
+if (true) {
+
+	NewTab('#', "");
+
+}
+});
+gv_vAlignTable['u47'] = 'center';gv_vAlignTable['u49'] = 'center';document.getElementById('u10_img').tabIndex = 0;
 
 u10.style.cursor = 'pointer';
 $axure.eventManager.click('u10', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
+	NewTab('#', "");
 
 }
 });
@@ -143,7 +143,7 @@ $axure.eventManager.click('u12', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
+	NewTab('#', "");
 
 }
 });
@@ -154,7 +154,7 @@ $axure.eventManager.click('u14', function(e) {
 
 if (true) {
 
-	NewTab($axure.globalVariableProvider.getLinkUrl('Page_1.html'), "");
+	NewTab('#', "");
 
 }
 });
